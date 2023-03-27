c5efd7ac1bec19d3c68938c9ff81e77c1b021bb0
loaywaleed <loaywaleed9@gmail.com>
2023-03-27 14:05:16 -0700

    Hot fix and added main.c test file

--- _printf.c
+++ _printf.c
@@ -14,31 +14,32 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; format[i]; i++)
 	{
-		if (format[i] = '%')
+		if (format[i] == '%')
 		{
-			return(-1);
-		}
-		switch (format[i + 1])
-		{
-			char temp;
-			char *string;
-			case 'c':
-			temp = va_arg(args, int);
-			put_char(temp);
-			break;
-			case 's':
-			string = va_arg(args, char *);
-			for (j = 0; string[j]; j++)
+			if (format[i + 1] == '\0')
+				return(-1);
+			switch (format[i + 1])
 			{
-				put_char(string[j]);
+				char temp;
+				char *string;
+				case 'c':
+				temp = va_arg(args, int);
+				put_char(temp);
+				break;
+				case 's':
+				string = va_arg(args, char *);
+				for (j = 0; string[j]; j++)
+				{
+					put_char(string[j]);
+				}
+				break;
 			}
-			break;
+			i++;
 		}
-		i++;
-	}
-	else
-	{
-		put_char(format[i]);
+		else
+		{
+			put_char(format[i]);
+		}
 	}
 	return (0);
 }
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+
+int main(void)
+{
+	char str = 'n';
+	_printf("hello, %c\n", str);
+	return (0);
+}
