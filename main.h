16bdb5a23fface2fe2c279c6c50e2f2cb3bc0799
Asser Elshafey <elshafeyasser@yahoo.com>
2023-03-27 13:42:48 -0700

    added function prototypes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int put_char(char character);
+int _printf(const char *format, ...);
+
+
+#endif /* MAIN_H */
