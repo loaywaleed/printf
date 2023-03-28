de0cdcf66ca2c8c8b8948b2d27ad831f198db6cc
loaywaleed <loaywaleed9@gmail.com>
2023-03-28 15:29:40 -0700

    Hot fix

--- main.h
+++ main.h
@@ -11,6 +11,6 @@ int print_char(va_list args);
 int print_string(va_list args);
 int print_default(const char *format, int index);
 char *int_to_str(int num);
-int print_int(valist args);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
